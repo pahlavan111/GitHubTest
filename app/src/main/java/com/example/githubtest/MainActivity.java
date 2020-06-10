4ff035a6b58684d247ae2967e8b62c8b121e5794
behrooz pahlavan <pahlavan111@gmail.com>
2020-06-10 10:42:22 +0430

    merged whit master

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
